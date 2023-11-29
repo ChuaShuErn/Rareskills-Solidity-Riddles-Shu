afa2229db29a510a34a83d9cdd687e79cfa7d17d
Chua, Shu Ern <shuernchua@kapps.com.sg>
2023-11-29 14:14:53 +0800

    Forwarder

--- contracts/Forwarder.sol
+++ contracts/Forwarder.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
+import "hardhat/console.sol";
+
 contract Wallet {
     address public immutable forwarder;
 
@@ -10,15 +12,23 @@ contract Wallet {
     }
 
     function sendEther(address destination, uint256 amount) public {
+        console.log("sendEther entered");
         require(msg.sender == forwarder, "sender must be forwarder contract");
-        (bool success, ) = destination.call{value: amount}("");
+        console.log("balance of this wallet:", address(this).balance);
+        console.log("require passed");
+        console.log("destination:", destination);
+        console.log("amount:", amount);
+        (bool success,) = destination.call{value: amount}("");
         require(success, "failed");
+        console.log("success sendEther:", success);
+        console.log("after transfer wallet balance:", address(this).balance);
     }
 }
 
 contract Forwarder {
     function functionCall(address a, bytes calldata data) public {
-        (bool success, ) = a.call(data);
+        console.log("functionCall entered");
+        (bool success,) = a.call(data);
         require(success, "forward failed");
     }
 }
